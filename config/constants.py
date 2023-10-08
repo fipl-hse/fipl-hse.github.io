32b00792d65b18ef0c804df97a238f2facd9ed63
Andrei K <logerk3@gmail.com>
2023-10-08 23:14:40 +0100

    rename

--- config/constants.py
+++ config/constants.py
@@ -8,7 +8,7 @@ from config.project_config import ProjectConfig
 PROJECT_ROOT = Path(__file__).parent.parent
 PROJECT_CONFIG_PATH = PROJECT_ROOT / 'config' / 'project_config.json'
 
-SOURCE_CODE_ROOT = PROJECT_ROOT.joinpath('code_source')
+SOURCE_CODE_ROOT = PROJECT_ROOT.joinpath('labs')
 
 DOC_BUILD_DIR = PROJECT_ROOT.joinpath('source').joinpath('build')
 WEBSITE_ROOT = PROJECT_ROOT.joinpath('source')
--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../code_source'))
+sys.path.insert(0, os.path.abspath('../labs'))
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'Лабораторный Практикум и Курс Лекций'
